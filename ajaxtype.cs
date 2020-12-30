17812db80cd2aa57431f80164bd89045d718ef60
Saikrishna-A <asaikrishna407@gmail.com>
2020-12-30 18:30:22 +0530

    html practice

---
+++ ajaxtype.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class ajaxtype()
+{
+	public void ajax()
+    {
+        if (Request.Form["a"] != null && Request.Form["b"] != null)
+        {
+            int a = int.Parse(Request.Form["a"]);
+            int b = int.Parse(Request.Form["b"]);
+            int sum = a + b;
+            console.Write("The Sum is " + sum);
+            // System.Threading.Thread.Sleep(5000);
+        }
+        return;
+    }
+}
